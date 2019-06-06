154ce92b598e95ec63f487a988ab4e68c2055fa1
Rhyan Foo Kune <rfookune@nextekdevelopment.com>
2019-06-06 14:28:21 -0500

    removed bootstrap

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container mx-auto">
     <div class="flex items-center mb-3">
         <h1 class="mr-auto">Birdoard</h1>
         <a href="/projects/create">New Project</a>
     </div>
-    <ul>
+
+    <div class="flex">
         @forelse($projects as $project)
-        <li>
-            <a href="{{ $project->path() }}">
-                {{ $project->title }}
-            </a>
-        </li>
+            <div class="bg-white mr-4 rounded shadow w-1/3 p-5">
+                <h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
+                <div class="text-gray-500">{{ str_limit($project->description, 200) }}</div>
+            </div>
         @empty
-        <li>No projects yet.</li>
+            <div>No projects yet.</div>
         @endforelse
-    </ul>
+    </div>
 </div>
 @endsection
